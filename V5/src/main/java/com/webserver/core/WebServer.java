f85d946d3165dba202fae7afffa8f07fcfd24019
zeminwen <583403980@qq.com>
2021-02-18 19:52:54 +0800

    v6

---
+++ V5/src/main/java/com/webserver/core/WebServer.java
@@ -0,0 +1,50 @@
+package com.webserver.core;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 小鸟WebServer
+ * 实现Tomcat的基础功能的一个Web容器
+ * Web容器的作用：
+ * 1.Web容器是一个Web服务端程序，负责与客户端(通常是浏览器)进行交互。
+ * 2.完成与客户端的TCP连接及数据交互。
+ * 3.基于HTTP协议与客户端进行应用交互，使得浏览器可以访问Web容器中部署的不同网络应用(Webapp)
+ * 的页面，资源，功能等。
+ * 4.可以管理部署多个不同的网络应用。
+ */
+public class WebServer {
+    private ServerSocket serverSocket;
+    public WebServer(){
+        try {
+            System.out.println("正在启动服务器...");
+            serverSocket=new ServerSocket(8088);
+            System.out.println("服务端启动完毕！");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void start(){
+        try {
+            /*
+             http://localhost:8088
+             http://localhost:8088/index.html
+             */
+            System.out.println("等待客户端连接...");
+            Socket socket=serverSocket.accept();
+            System.out.println("一个客户端连接了！");
+            //启动一个线程与该客户端交互
+            ClientHandler handler=new ClientHandler(socket);
+            Thread t=new Thread(handler);
+            t.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        WebServer server=new WebServer();
+        server.start();
+    }
+}
--- V5/src/main/java/com/webserver/http/HttpRequest.java
+++ V5/src/main/java/com/webserver/http/HttpRequest.java
@@ -1,4 +1,4 @@
-package com.webserver.core;
+package com.webserver.http;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,30 +7,43 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 负责与指定客户端进行HTTP交互
- * HTTP协议要求与客户端的交互规则采取一问一答的方式。因此，处理客户端交互以三步形式完成：
- * 1.解析请求(一问)
- * 2.处理请求
- * 3.发送响应(一答)
+ * 请求对象
+ * 该类的每一个实例用于表示客户端发送过来的一个HTTP请求内容
+ * 每个请求由三部分组成：
+ * 请求行、消息头、消息正文
  */
-public class ClientHandler implements Runnable{
+public class HttpRequest {
+    //请求行相关信息
+    private String method;//请求方式
+    private String uri;//抽象路径
+    private String protocol;//协议版本
+
+    //消息头相关信息
+    private Map<String,String> headers=new HashMap<>();
+    //消息正文相关信息
+
     private Socket socket;
-    public ClientHandler(Socket socket){
+
+    /**
+     * HttpRequest的实例化过程就是解析请求的过程
+     * @param socket
+     */
+    public HttpRequest(Socket socket){
         this.socket=socket;
+        //1.解析请求行
+        parseRequestLine();
+        //2.解析消息头
+        parseHeadlers();
+        //3.解析消息正文
+        parseContent();
     }
-    @Override
-    public void run() {
+    //解析一个请求的三步骤：
+    //1：解析请求行
+    private void parseRequestLine(){
+        System.out.println("HttpRequest:开始解析请求行...");
         try{
-            //1解析请求
-            InputStream in=socket.getInputStream();
-            //测试读取客户端发送过来的请求内容
             String line=readLine();
             System.out.println("请求行:"+line);
-            String method;//请求方式
-            String uri;//抽象路径
-            String protocol;//协议版本
-
-
             //http://localhost:8088/index.html
             //将请求行按照空格拆分为三部分，并分别赋值给上述变量
             String[]data=line.split("\\s");
@@ -44,36 +57,38 @@ public class ClientHandler implements Runnable{
             System.out.println("method:"+method);
             System.out.println("uri:"+uri);
             System.out.println("protocol:"+protocol);
-
-            //读取所有消息头
-            Map<String,String>headers=new HashMap<>();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        System.out.println("HttpRequest:请求行解析完成");
+    }
+    //2：解析消息头
+    private void parseHeadlers(){
+        System.out.println("HttpRequest:开始解析消息头...");
+        try{
             //下面读取每一个消息头，将消息头的名字作为key，消息头的值作为value保存到headers中
             while (true) {
-                line = readLine();
+                String line = readLine();
                 //读取消息头时，如果只读取到了回车加换行符就应当停止读取e
                 if (line.isEmpty()){//readLine单独读取CRLF返回值应当是空字符串
                     break;
                 }
                 System.out.println("消息头:" + line);
                 //将消息头按照冒号空格拆分并存入到headers
-                data=line.split(":\\s");
+                String[] data=line.split(":\\s");
                 headers.put(data[0],data[1]);
             }
             System.out.println("headers:"+headers);
-            //2处理请求
-
-            //3发送响应
-
-        }catch (Exception e){
+        }catch (IOException e){
             e.printStackTrace();
-        }finally {
-            //处理完毕后与客户端断开连接
-            try{
-                socket.close();
-            }catch (IOException e){
-                e.printStackTrace();
-            }
         }
+        System.out.println("HttpRequest:消息头解析完成");
+    }
+    //3：解析消息正文
+    private void parseContent(){
+        System.out.println("HttpRequest:开始解析消息正文...");
+
+        System.out.println("HttpRequest:消息正文解析完成");
     }
 
     public String readLine() throws IOException {
@@ -96,4 +111,20 @@ public class ClientHandler implements Runnable{
         }
         return builder.toString().trim();//用trim()删除最后拼上的回车符
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
